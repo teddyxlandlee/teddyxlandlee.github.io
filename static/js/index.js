780416ac74842fe5772c5436d24be302c27bcfee
teddyxlandlee <tedexe_work.top@foxmail.com>
2020-08-14 17:51:02 +0800

    Commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,1 @@
-var link = "https://github.com/teddyxlandlee/sweet_potato";
-if (navigator.language != "zh-CN" || navigator.language != "zh-TW") 
-	link = "https://www.mcbbs.net/thread-1086214-1-1.html"
-
-function link() {
-	window.location = link;
-}
+window.location = "/wiki/sweet_potato_mod/index.html";
